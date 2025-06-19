c47e451ec329a6f6627d30762a41d19e87bd9b57
prakash kumawat <h2298806@gmail.com>
2025-06-19 22:27:54 +0530

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function git(argv) {
-    console.log(argv);
-}
-
-git("hello prakash");
-
-class prakash {
-    constructor(name,classname,subject) {
-        this.name = name;
-        this.classname = classname;
-        this.subject = subject;
-    }
-}
-
-const myclass = new prakash("prakash","12","computer");
-
-console.log(myclass);
+console.log("hy");
